2dfc850d45057121b88fc501db1ec2f1bdf45db1
Sidney Cadot <sidney@jigsaw.nl>
2016-11-05 23:25:13 +0100

    Added references sub-directory.

---
+++ references/get-references.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+# Atmel SAME70 microcontrollers
+wget http://www.atmel.com/Images/Atmel-11296-32-bit-Cortex-M7-Microcontroller-SAM-E70Q-SAM-E70N-SAM-E70J_Datasheet.pdf
+
+# Atmel SAME70 evaluation board (XPlained)
+wget http://www.atmel.com/Images/Atmel-44050-Cortex-M7-Microcontroller-SAM-E70-XPLD-Xplained_User-guide.pdf
+wget http://www.atmel.com/Images/SAME70-XPLD-KitsFiles-Readme.pdf
+wget http://www.atmel.com/images/SAME70-XPLD__KitsFiles.zip
+
+# Ethernet transceiver.
+# SAME70 Xplained board has a KSZ2081RNACA.
+
+wget http://ww1.microchip.com/downloads/en/DeviceDoc/00002199A.pdf
